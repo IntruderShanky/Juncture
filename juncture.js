8005614d7ad3d68660819a497f7df61f7f5b2059
intrudershanky <intrudershanky@outlook.com>
2018-10-31 17:46:48 +0530

    Trigger only Once

--- juncture.js
+++ juncture.js
@@ -1,6 +1,7 @@
 var elementId = '';
 var elementClass = '';
 var elements = [];
+var trigger = [];
 
 var offset = 0;
 
@@ -11,28 +12,36 @@ function Juncture(options, handler) {
     if (options["elementClass"]) {
         elementClass = options["elementClass"];
     }
-    if(options["offset"]){
+    if (options["offset"]) {
         offset = options["offset"]
     }
 
     window.addEventListener('load', function () {
+        elements = [];
+        trigger = [];
         if (elementId.length > 0) {
             const element = document.getElementById(elementId);
             elements.push(element);
+            trigger.push(true);
         }
         if (elementClass.length > 0) {
             const ele = document.getElementsByClassName(elementClass);
-            for(const e of ele) {
+            for (const e of ele) {
                 elements.push(e);
+                trigger.push(true)
             }
         }
         console.log(elements);
     });
 
     window.addEventListener('scroll', function () {
-        for (const item of elements) {
+        for (var i = 0; i < elements.length; i++) {
+            const item = elements[i];
             if ((item.offsetTop - offset) < (window.pageYOffset + window.innerHeight)) {
-                handler(item);
+                if (trigger[i]) {
+                    handler(item);
+                    trigger[i] = false;
+                }
             }
         }
     });
